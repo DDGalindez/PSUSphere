b42402c85ff87660f8b1204b0a49e379fbcb326d
DDGalindez <142036344+DDGalindez@users.noreply.github.com>
2024-10-31 16:32:25 +0800

    update host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+
