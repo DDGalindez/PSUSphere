f9aacd1eb425a74575b86b23dc2168e0b66ccef6
DDGalindez <142036344+DDGalindez@users.noreply.github.com>
2024-10-31 16:37:39 +0800

    yawa ayaw gumana

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -124,4 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['deyvuu.pythonanywhere.com', '127.0.0.1']
 
