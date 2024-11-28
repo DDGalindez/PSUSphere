dc689cdb3f6ec331ed4b6d0fe7a982e0e720e05e
DDGalindez <142036344+DDGalindez@users.noreply.github.com>
2024-11-28 14:58:51 +0800

    message

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization, OrgMember, Student, Program, College,Boat
+from studentorg.models import Organization, OrgMember, Student, Program, College, Boat
 from studentorg.forms import OrganizationForm, OrgMemberForm, StudentForm, ProgramForm, CollegeForm
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -15,24 +15,22 @@ from django.db.models import Q
 # Create your views here.
 
 @method_decorator(login_required, name='dispatch')
-
 class HomePageView(ListView):
-    model = Student 
+    model = Student
     context_object_name = 'home'
     template_name = "home.html"
 
 class Organizationlist(ListView):
     model = Organization
-    content_object_name = 'organization'
+    context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
-    
+
     def get_queryset(self, *args, **kwargs):
-        qs = super(Organizationlist, self).get_queryset(*args, **kwargs)
+        qs = super().get_queryset(*args, **kwargs)
         if self.request.GET.get("q") != None:
             query = self.request.GET.get('q')
-            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query)|
-                           Q(college__college_name__icontains=query))
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(college__college_name__icontains=query))
         return qs
 
 class OrganizationCreateView(CreateView):
@@ -59,7 +57,6 @@ class OrganizationUpdateView(UpdateView):
 
 class OrganizationDeleteView(DeleteView):
     model = Organization
-#    form_class = OrganizationForm
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
 
@@ -69,17 +66,17 @@ class OrganizationDeleteView(DeleteView):
 
 class OrgMemberlist(ListView):
     model = OrgMember
-    content_object_name = 'orgmember'
+    context_object_name = 'orgmember'
     template_name = 'orgmember_list.html'
     paginate_by = 5
     ordering = ['date_joined']
 
     def get_queryset(self, *args, **kwargs):
-        qs = super(OrgMemberlist, self).get_queryset(*args, **kwargs)
+        qs = super().get_queryset(*args, **kwargs)
         if self.request.GET.get("q") != None:
             query = self.request.GET.get('q')
-            qs = qs.filter(Q(date_joined__icontains=query)|Q(student__firstname__icontains=query)|
-                           Q(student__lastname__icontains=query)|Q(organization__name__icontains=query))
+            qs = qs.filter(Q(date_joined__icontains=query) | Q(student__firstname__icontains=query) |
+                           Q(student__lastname__icontains=query) | Q(organization__name__icontains=query))
         return qs
 
 class OrgMemberCreateView(CreateView):
@@ -106,7 +103,6 @@ class OrgMemberUpdateView(UpdateView):
 
 class OrgMemberDeleteView(DeleteView):
     model = OrgMember
-#    form_class = OrgMemberForm
     template_name = 'orgmember_del.html'
     success_url = reverse_lazy('orgmember-list')
 
@@ -116,17 +112,17 @@ class OrgMemberDeleteView(DeleteView):
 
 class StudentList(ListView):
     model = Student
-    content_object_name = 'student'
+    context_object_name = 'student'
     template_name = 'student_list.html'
     paginate_by = 5
 
     def get_queryset(self, *args, **kwargs):
-        qs = super(StudentList, self).get_queryset(*args, **kwargs)
+        qs = super().get_queryset(*args, **kwargs)
         if self.request.GET.get("q") != None:
             query = self.request.GET.get('q')
             qs = qs.filter(Q(student_id__icontains=query) | Q(firstname__icontains=query) |
-                            Q(lastname__icontains=query) | Q(middlename__icontains=query) |
-                            Q(program__prog_name__icontains=query))
+                           Q(lastname__icontains=query) | Q(middlename__icontains=query) |
+                           Q(program__prog_name__icontains=query))
         return qs
 
 class StudentCreateView(CreateView):
@@ -153,7 +149,6 @@ class StudentUpdateView(UpdateView):
 
 class StudentDeleteView(DeleteView):
     model = Student
-#    form_class = StudentForm
     template_name = 'student_del.html'
     success_url = reverse_lazy('student-list')
 
@@ -161,18 +156,17 @@ class StudentDeleteView(DeleteView):
         messages.success(self.request, f"Student Deleted successfully.")
         return super().form_valid(form)
 
-
 class ProgramList(ListView):
     model = Program
-    content_object_name = 'program'
+    context_object_name = 'program'
     template_name = 'program_list.html'
     paginate_by = 5
 
     def get_queryset(self, *args, **kwargs):
-        qs = super(ProgramList, self).get_queryset(*args, **kwargs)
+        qs = super().get_queryset(*args, **kwargs)
         if self.request.GET.get("q") != None:
             query = self.request.GET.get('q')
-            qs = qs.filter(Q(prog_name__icontains=query)|Q(college__college_name__icontains=query))
+            qs = qs.filter(Q(prog_name__icontains=query) | Q(college__college_name__icontains=query))
         return qs
 
 class ProgramCreateView(CreateView):
@@ -199,7 +193,6 @@ class ProgramUpdateView(UpdateView):
 
 class ProgramDeleteView(DeleteView):
     model = Program
-#    form_class = ProgramForm
     template_name = 'program_del.html'
     success_url = reverse_lazy('program-list')
 
@@ -209,12 +202,12 @@ class ProgramDeleteView(DeleteView):
 
 class CollegeList(ListView):
     model = College
-    content_object_name = 'college'
+    context_object_name = 'college'
     template_name = 'college_list.html'
     paginate_by = 5
 
     def get_queryset(self, *args, **kwargs):
-        qs = super(CollegeList, self).get_queryset(*args, **kwargs)
+        qs = super().get_queryset(*args, **kwargs)
         if self.request.GET.get("q") != None:
             query = self.request.GET.get('q')
             qs = qs.filter(Q(college_name__icontains=query))
@@ -234,19 +227,16 @@ class CollegeCreateView(CreateView):
 class CollegeUpdateView(UpdateView):
     model = College
     fields = "__all__"
-    context_object_name = "college"
     template_name = 'college_edit.html'
     success_url = reverse_lazy('college-list')
 
     def form_valid(self, form):
         college_name = form.instance.college_name
-        messages.success(self.request,f'{college_name} has been Updated.')
-
+        messages.success(self.request, f"{college_name} has been Updated.")
         return super().form_valid(form)
 
 class CollegeDeleteView(DeleteView):
     model = College
-    #form_class = CollegeForm
     template_name = 'college_del.html'
     success_url = reverse_lazy('college-list')
 
@@ -254,32 +244,32 @@ class CollegeDeleteView(DeleteView):
         messages.success(self.request, f"College Deleted successfully.")
         return super().form_valid(form)
 
+# Boat Views
 class BoatCreateView(CreateView):
     model = Boat
     fields = "__all__"
     template_name = "boat_form.html"
     success_url = reverse_lazy('boat-list')
-    
+
     def post(self, request, *args, **kwargs):
         length = request.POST.get('length')
         width = request.POST.get('width')
         height = request.POST.get('height')
-        
-        # Validate dimensions
+
+        # Validate the dimensions
         errors = []
-        for field_name, value in [('length', length), ('width', width), ('height', height)]: 
+        for field_name, value in [('length', length), ('width', width), ('height', height)]:
             try:
                 if float(value) <= 0:
                     errors.append(f"{field_name.capitalize()} must be greater than 0.")
             except (ValueError, TypeError):
                 errors.append(f"{field_name.capitalize()} must be a valid number.")
-            
-        # If errors exist, display them and return to the form
+
         if errors:
             for error in errors:
                 messages.error(request, error)
             return self.form_invalid(self.get_form())
-        # Call the parent's post() if validation passes
+        
         return super().post(request, *args, **kwargs)
 
 class BoatUpdateView(UpdateView):
@@ -287,26 +277,25 @@ class BoatUpdateView(UpdateView):
     fields = "__all__"
     template_name = "boat_form.html"
     success_url = reverse_lazy('boat-list')
-    
+
     def post(self, request, *args, **kwargs):
         length = request.POST.get('length')
         width = request.POST.get('width')
         height = request.POST.get('height')
-        
-        # Validate dimensions
+
+        # Validate the dimensions
         errors = []
-        for field_name, value in [('length', length), ('width', width), ('height', height)]: 
+        for field_name, value in [('length', length), ('width', width), ('height', height)]:
             try:
                 if float(value) <= 0:
                     errors.append(f"{field_name.capitalize()} must be greater than 0.")
             except (ValueError, TypeError):
                 errors.append(f"{field_name.capitalize()} must be a valid number.")
- 
-        # If errors exist, display them and return to the form
+
         if errors:
             for error in errors:
                 messages.error(request, error)
             return self.form_invalid(self.get_form())
         
-        # Call the parent's post() if validation passes
         return super().post(request, *args, **kwargs)
+
